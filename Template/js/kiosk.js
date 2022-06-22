70791e8eb1cfe8b02f181909692c0d1510c2010a
christoph <christoph@7754d8ab-9b54-e011-a251-0001c006bdc7>
2022-06-22 22:57:48 +0000

    Kiosk mode: add an input field for ext id and hide all other inputs
    git-svn-id: https://localhost/svn/WebGen/trunk@8406 7754d8ab-9b54-e011-a251-0001c006bdc7

---
+++ Template/js/kiosk.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    let scanner = '<div><input id="scanner" type="text"></div>';
+    $('div#content').after($(scanner));
+    $('#scanner').on('change', function() {
+        // Reload the page with new filter
+        window.location.href = window.location.href.split('?')[0] + '?nav=reports&filterExtId=' + $('#scanner').val();
+    });
+    
+    // Wait a bit until page contnt is loaded , too, before showing the rows
+    setTimeout(function() {$('table tbody').not('.filtered').find('tr').collapse('show');}, 100);
+});
