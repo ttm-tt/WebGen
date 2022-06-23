f0430faea716191a7af0cc79b5e91b9530502c61
christoph <christoph@7754d8ab-9b54-e011-a251-0001c006bdc7>
2022-06-23 07:46:47 +0000

    Keep scanner input focused
    git-svn-id: https://localhost/svn/WebGen/trunk@8407 7754d8ab-9b54-e011-a251-0001c006bdc7

--- Template/js/kiosk.js
+++ Template/js/kiosk.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
     let scanner = '<div><input id="scanner" type="text"></div>';
     $('div#content').after($(scanner));
+    $('#scanner').focus();
+    $('#scanner').on('blur', function(e) {
+        e.preventDefault();
+        $(this).focus();
+    });
     $('#scanner').on('change', function() {
         // Reload the page with new filter
         window.location.href = window.location.href.split('?')[0] + '?nav=reports&filterExtId=' + $('#scanner').val();
